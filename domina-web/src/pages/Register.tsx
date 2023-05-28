0db4bcce5896e3d1070d78f004021fed8464ea4b
Melissa Cadavid Pelaez <mcada01@yahoo.com.mx>
2023-05-28 11:20:01 -0500

    center the register form

--- domina-web/src/pages/Register.tsx
+++ domina-web/src/pages/Register.tsx
@@ -25,80 +25,84 @@ const Register: React.FC = () => {
   return (
     <>
       <div className="container vh-100">
-        <div
-          className="card o-hidden border-0 shadow-lg my-5"
-          style={{ width: "800px" }}
-        >
-          <div className="card-body p-0">
-            <div className="row">
-              <div className="p-5">
-                <div className="text-left">
-                  <h1 className="h4 text-gray-900 mb-4">Registrar usuario</h1>
+        <div className="row vh-100 justify-content-center align-items-center">
+          <div
+            className="card o-hidden border-0 shadow-lg my-5"
+            style={{ width: "800px" }}
+          >
+            <div className="card-body p-0">
+              <div className="row">
+                <div className="p-5">
+                  <div className="text-left">
+                    <h1 className="h4 text-gray-900 mb-4">Registrar usuario</h1>
+                  </div>
+                  {error && <div className="alert alert-danger">{error}</div>}
+                  <form onSubmit={handleRegister}>
+                    <div className="row">
+                      <div className="col s12 m6">
+                        <div className="form-group mb-2">
+                          <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Nombre"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            required
+                          />
+                        </div>
+                      </div>
+                      <div className="col s12 m6">
+                        <div className="form-group mb-2">
+                          <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Apellido"
+                            value={lastname}
+                            onChange={(e) => setLastname(e.target.value)}
+                            required
+                          />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="row">
+                      <div className="col s12 m6">
+                        <div className="form-group mb-2">
+                          <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Usuario"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                            required
+                          />
+                        </div>
+                      </div>
+                      <div className="col s12 m6">
+                        <div className="form-group mb-2">
+                          <input
+                            type="password"
+                            className="form-control"
+                            placeholder="Contraseña"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                          />
+                        </div>
+                      </div>
+                    </div>
+                    <div style={{ textAlign: "right" }}>
+                      <button
+                        className="btn btn-secondary m-2"
+                        onClick={() => navigate("/login", { replace: true })}
+                      >
+                        Cerrar
+                      </button>
+                      <button type="submit" className="btn btn-primary">
+                        Guardar
+                      </button>
+                    </div>
+                  </form>
                 </div>
-                {error && <div className="alert alert-danger">{error}</div>}
-                <form onSubmit={handleRegister}>
-                  <div className="row">
-                    <div className="col s12 m6">
-                      <div className="form-group mb-2">
-                        <input
-                          type="text"
-                          className="form-control"
-                          placeholder="Nombre"
-                          value={name}
-                          onChange={(e) => setName(e.target.value)}
-                          required
-                        />
-                      </div>
-                    </div>
-                    <div className="col s12 m6">
-                      <div className="form-group mb-2">
-                        <input
-                          type="text"
-                          className="form-control"
-                          placeholder="Apellido"
-                          value={lastname}
-                          onChange={(e) => setLastname(e.target.value)}
-                          required
-                        />
-                      </div>
-                    </div>
-                  </div>
-                  <div className="row">
-                    <div className="col s12 m6">
-                      <div className="form-group mb-2">
-                        <input
-                          type="text"
-                          className="form-control"
-                          placeholder="Usuario"
-                          value={username}
-                          onChange={(e) => setUsername(e.target.value)}
-                          required
-                        />
-                      </div>
-                    </div>
-                    <div className="col s12 m6">
-                      <div className="form-group mb-2">
-                        <input
-                          type="password"
-                          className="form-control"
-                          placeholder="Contraseña"
-                          value={password}
-                          onChange={(e) => setPassword(e.target.value)}
-                          required
-                        />
-                      </div>
-                    </div>
-                  </div>
-                  <div style={{ textAlign: 'right' }}
-                  >
-                    <button className="btn btn-secondary m-2" onClick={() => navigate("/login", { replace: true })}>
-                      Cerrar
-                    </button>
-                    <button type="submit" className="btn btn-primary">
-                      Guardar
-                    </button>
-                  </div>
-                </form>
               </div>
             </div>
           </div>
